3c8c439114ac0f64e1f6d58dffaca5f7700e5097
flvoyer <26801813+flvoyer@users.noreply.github.com>
2022-10-12 21:44:32 +0200

    refactor(js): removed unused lines

--- js/app.js
+++ js/app.js
@@ -1,6 +1,18 @@
+
+initPoints();
+
+setInterval(function() {
+    let finding = document.querySelector(".claimable-bonus__icon");
+
+    if (finding) {
+        finding.click();
+        addPoints();
+    }
+}, 10000);
+
 /**
  * Error display
- * @param {*} error 
+ * @param {*} error
  */
 function onError(error) {
 	console.error(`Error: ${error}`);
@@ -10,65 +22,34 @@ function onError(error) {
  * Check for points storage existence and if not, set points to 0
  */
 function initPoints() {
-    
-    browser.storage.sync.get("points")
+    browser.storage.sync.get('points')
     .then( function(data) {
-        
-        if(!data.points) {
-
-            browser.storage.sync.set({points: 0});
-
-        }
-
-    }, onError);
-
+    if(!data.points) {
+        browser.storage.sync.set({points: 0});
+    }
+}, onError);
 }
 
 /**
  * Get the actual amount of points and adds 50
  */
 function addPoints() {
-
     var interval = setInterval( function() {
-
         var ptsWon = document.querySelector(".pulse-animation .tw-c-text-link").textContent;
-
         if (ptsWon) {
-
             clearInterval(interval);
-            
+
             // Remove the plus sign in "+50 / +100"
             ptsWon = ptsWon.substring(1);
 
-            browser.storage.sync.get("points")
+            browser.storage.sync.get('points')
             .then( function(data) {
-                
                 browser.storage.sync.set({
-
                     points: parseFloat(data.points) + parseFloat(ptsWon)
-                    
                 }).then( () => {
                     console.log(`[AutoTwitchPoints] +${ptsWon}`);
                 });
-
             });
-
         }
     }, 1000);
-
 }
-
-/**
- * Start point
- */
-initPoints();
-
-setInterval(function() {
-    let finding = document.querySelector(".claimable-bonus__icon");
-    
-    if (finding) {
-        finding.click();        
-        addPoints();
-    }
-
-}, 10000);
